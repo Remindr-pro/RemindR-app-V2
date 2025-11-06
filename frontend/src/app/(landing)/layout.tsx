8b6898c26f42a12f7dc7ef49afd0feebd34f1777
nathanbaa <contact.nathanbastard@gmail.com>
2025-11-06 16:25:50 +0100

    fix(landing): update layout by using footer and navbar component

--- frontend/src/app/(landing)/layout.tsx
+++ frontend/src/app/(landing)/layout.tsx
@@ -1,7 +1,16 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 export default function LandingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
 }
