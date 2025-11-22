78817d8e7f410e4fab603e636ab403d00c0615d2
nathanbaa <contact.nathanbastard@gmail.com>
2025-11-23 00:49:55 +0100

    feat(page-professionnels): add hero section

--- frontend/src/app/(landing)/professionnels/page.tsx
+++ frontend/src/app/(landing)/professionnels/page.tsx
@@ -1,7 +1,24 @@
+import Button from "@/app/components/Button";
+
 export default function ProfessionnelsPage() {
   return (
-    <div>
-      <h1>Professionnels</h1>
-    </div>
+    <>
+      {/* === Section Hero === */}
+      <section className="min-h-screen flex flex-col items-center justify-center gap-8 px-5 lg:px-0 max-w-full md:max-w-3/4 mx-auto text-center">
+        <h1 className="text-3xl md:text-6xl font-inclusive font-bold">
+          Ensemble, faisons de la prévention une force pour votre mutuelle.
+        </h1>
+        <p className="text-base md:text-lg font-inclusive font-medium">
+          Remindr valorise votre engagement santé avec une solution
+          personnalisée à l’image de votre mutuelle. En marque blanche, elle
+          renforce le bien-être de vos adhérents et optimise vos coûts.
+        </p>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+          <Button href="/professionnels/contact" variant="green">
+            Découvrir la solution Remindr
+          </Button>
+        </div>
+      </section>
+    </>
   );
 }
