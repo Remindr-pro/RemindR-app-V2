8a2fa09db53e4640de0a362ba2ddd418acf2e0bc
nathanbaa <contact.nathanbastard@gmail.com>
2025-11-24 10:44:08 +0100

    feat(page-magazine): add hero section

--- frontend/src/app/(landing)/particuliers/magazine/page.tsx
+++ frontend/src/app/(landing)/particuliers/magazine/page.tsx
@@ -1,7 +1,18 @@
 export default function MagazinePage() {
   return (
-    <div>
-      <h1>Magazine</h1>
-    </div>
+    <>
+      {/* === Section Hero === */}
+      <section className="min-h-screen flex flex-col items-center justify-center gap-8 px-5 lg:px-0 max-w-full md:max-w-3/4 mx-auto text-center">
+        <div className="max-w-2xl mx-auto w-full flex flex-col items-center justify-center gap-4">
+          <h1 className="text-3xl md:text-6xl font-inclusive font-bold">
+            Le magazine prévention Santé.
+          </h1>
+          <p className="text-base md:text-lg font-inclusive font-medium">
+            Prenez soin de vous et de vos proches grâce à des informations
+            claires, basées sur les recommandations officielles.
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
