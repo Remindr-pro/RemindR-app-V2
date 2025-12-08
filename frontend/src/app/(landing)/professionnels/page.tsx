5c6efa7cfb93a25826c0f5d605424bdac6230a28
nathanbaa <contact.nathanbastard@gmail.com>
2025-12-08 16:56:06 +0100

    feat(professionnels-page): update hero section text and layout for improved engagement

--- frontend/src/app/(landing)/professionnels/page.tsx
+++ frontend/src/app/(landing)/professionnels/page.tsx
@@ -4,18 +4,21 @@ export default function ProfessionnelsPage() {
   return (
     <>
       {/* === Section Hero === */}
-      <section className="min-h-screen flex flex-col items-center justify-center gap-8 px-5 lg:px-0 max-w-full md:max-w-3/4 mx-auto text-center">
+      <section className="min-h-screen flex flex-col items-center justify-center gap-8 px-5 lg:px-0 max-w-full md:max-w-3/5 mx-auto text-center">
         <h1 className="text-3xl md:text-6xl font-inclusive font-bold">
-          Ensemble, faisons de la prévention une force pour votre mutuelle.
+          La prévention familiale, réinventée pour vos adhérents.
         </h1>
         <p className="text-base md:text-lg font-inclusive font-medium">
-          Remindr valorise votre engagement santé avec une solution
-          personnalisée à l’image de votre mutuelle. En marque blanche, elle
-          renforce le bien-être de vos adhérents et optimise vos coûts.
+          Un accompagnement santé personnalisé, évolutif et engageant, qui aide
+          vos adhérents à adopter les bons réflexes au bon moment et vous permet
+          de réduire vos coûts tout en renforçant la fidélisation.
         </p>
         <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+          <Button href="/professionnels/inscription" variant="dark">
+            Télécharger la documentation
+          </Button>
           <Button href="/professionnels/contact" variant="green">
-            Découvrir la solution Remindr
+            Demander une démo
           </Button>
         </div>
       </section>
