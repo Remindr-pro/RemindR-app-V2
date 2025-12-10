485e44c3881544bdac88dbf8c75bfd43fb55ccc3
nathanbaa <contact.nathanbastard@gmail.com>
2025-12-10 19:28:47 +0100

    feat(connexion): enhance bienvenue page with image, button, and link for improved user engagement

--- src/app/(connexion)/(activations)/bienvenue/page.tsx
+++ src/app/(connexion)/(activations)/bienvenue/page.tsx
@@ -1,7 +1,28 @@
+import Button from "@/app/components/atoms/Button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function BienvenuePage() {
   return (
-    <div className="w-full max-w-md">
-      {/* Contenu de la page de bienvenue */}
+    <div className="w-full max-w-md flex flex-col items-center justify-center gap-2 font-inclusive">
+      <Image
+        src="/images/icons/alarm.png"
+        alt="Alarme icon"
+        width={100}
+        height={100}
+        className="w-16 h-16 md:w-[100px] md:h-[100px]"
+      />
+      <span className="mb-4 text-xs">5 minutes pour l’activer !</span>
+      <Button variant="green" href="/identification">
+        Commencer
+      </Button>
+      <span>
+        Je peux aussi{" "}
+        <Link href="/" className="text-greenMain underline">
+          refuser l’accés
+        </Link>{" "}
+        à mon espace.
+      </span>
     </div>
   );
 }
